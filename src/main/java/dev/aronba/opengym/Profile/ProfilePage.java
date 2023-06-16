1b6102b0f6ca1a159c8b634fc86096551e0609d8
raphael-gi <raphael.gisler@stud.edubs.ch>
2023-06-17 01:41:14 +0200

    Create Profile Page

---
+++ src/main/java/dev/aronba/opengym/Profile/ProfilePage.java
@@ -0,0 +1,9 @@
+package dev.aronba.opengym.Profile;
+
+import javax.swing.*;
+
+public class ProfilePage extends JPanel {
+    public ProfilePage() {
+        add(new JLabel("heheheha"));
+    }
+}
--- src/main/java/dev/aronba/opengym/Window.java
+++ src/main/java/dev/aronba/opengym/Window.java
@@ -2,14 +2,13 @@ package dev.aronba.opengym;
 
 import dev.aronba.opengym.Calculator.CalculatorPage;
 import dev.aronba.opengym.History.HistoryPage;
+import dev.aronba.opengym.Profile.ProfilePage;
 import dev.aronba.opengym.Workout.WorkoutPage;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JFrame {
-
-
 
     static Window instance;
 
@@ -26,28 +25,22 @@ public class Window extends JFrame {
     private Window(){
         setLayout(new BorderLayout());
         setBounds(100,100,600,400);
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/logo.jpg")));
 
         WorkoutPage workoutPage = new WorkoutPage();
         HistoryPage historyPage = new HistoryPage();
         CalculatorPage calculatorPage = new CalculatorPage();
-
+        ProfilePage profilePage = new ProfilePage();
 
         JTabbedPane Navbar = new JTabbedPane();
-
 
         Navbar.addTab("Workout",workoutPage);
         Navbar.addTab("History",historyPage);
         Navbar.addTab("Calculator",calculatorPage);
+        Navbar.addTab("Profile",profilePage);
 
         add(Navbar);
 
-
-
-
-
         setVisible(true);
     }
-
-
-
 }
