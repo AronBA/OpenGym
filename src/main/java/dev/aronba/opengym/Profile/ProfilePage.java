b2b01d642f93a9347e41e83d202e6549b98943de
raphael-gi <raphael.gisler@stud.edubs.ch>
2023-06-17 02:41:13 +0200

    Switch themes through dropdown

--- src/main/java/dev/aronba/opengym/Profile/ProfilePage.java
+++ src/main/java/dev/aronba/opengym/Profile/ProfilePage.java
@@ -1,9 +1,34 @@
 package dev.aronba.opengym.Profile;
 
+import com.formdev.flatlaf.*;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import dev.aronba.opengym.Window;
+
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfilePage extends JPanel {
     public ProfilePage() {
-        add(new JLabel("heheheha"));
+        Map<String, FlatLaf> flatLafMap = new HashMap<>();
+        flatLafMap.put(FlatDarculaLaf.NAME, new FlatDarculaLaf());
+        flatLafMap.put(FlatIntelliJLaf.NAME, new FlatIntelliJLaf());
+        flatLafMap.put(FlatMacDarkLaf.NAME, new FlatMacDarkLaf());
+        flatLafMap.put(FlatMacLightLaf.NAME, new FlatMacLightLaf());
+        flatLafMap.put(FlatLightLaf.NAME, new FlatLightLaf());
+        flatLafMap.put(FlatDarkLaf.NAME, new FlatDarkLaf());
+        JComboBox<String> themesComboBox = new JComboBox(flatLafMap.keySet().toArray());
+        themesComboBox.addActionListener(l -> {
+            try {
+                System.out.println();
+                UIManager.setLookAndFeel(flatLafMap.get(themesComboBox.getSelectedItem().toString()));
+                SwingUtilities.updateComponentTreeUI(Window.getInstance());
+            } catch( Exception ex ) {
+                System.err.println( "Failed to switch LaF" );
+            }
+        });
+        add(themesComboBox);
     }
 }
